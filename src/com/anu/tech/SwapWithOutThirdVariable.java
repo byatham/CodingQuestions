0775576265e80d4581b1ae5383d573b4df85e708
byatham <balaiahy23@gmail.com>
2024-04-27 22:24:15 +0530

    code modified

---
+++ src/com/anu/tech/SwapWithOutThirdVariable.java
@@ -0,0 +1,32 @@
+package com.anu.tech;
+
+public class SwapWithOutThirdVariable {
+public int temp=0;
+	
+	public  void swapNumbers(int a, int b)
+	{
+		
+		System.out.println("before swap a,b :"+a+" ,"+b);
+		try {
+			a=a+b;
+			b=a-b;
+			a=a-b;
+			
+			
+			System.out.println("after swap a,b :"+a+" , "+b);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwapWithOutThirdVariable obj=new SwapWithOutThirdVariable();
+		obj.swapNumbers(10, 20);
+
+	}
+
+}
---
+++ src/com/anu/tech/SwapWithThirdVariable.java
@@ -0,0 +1,35 @@
+package com.anu.tech;
+
+public class SwapWithThirdVariable {
+	
+	public int temp=0;
+	
+	public  void swapNumbers(int a, int b)
+	{
+		
+		System.out.println("before swap a,b :"+a+" ,"+b);
+		try {
+			temp=a;
+			a=b;
+			b=temp;
+			System.out.println("after swap a,b :"+a+" , "+b);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SwapWithThirdVariable obj=new SwapWithThirdVariable();
+		obj.swapNumbers(20, 50);
+		
+		
+		
+
+	}
+
+}
